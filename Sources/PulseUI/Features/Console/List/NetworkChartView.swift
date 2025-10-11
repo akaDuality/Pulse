2a5a35007e4cbaba09b05a3f89f7bff1ebb442b9
Mikhail Rubanov <akaDuality@yandex.ru>
2025-10-11 16:46:32 -0300

    Fix build

--- Sources/PulseUI/Features/Console/List/NetworkChartView.swift
+++ Sources/PulseUI/Features/Console/List/NetworkChartView.swift
@@ -99,8 +99,8 @@ struct RequestRow: ChartContent {
             }
         }
         .annotation(position: .trailing, alignment: .center) {
-            if showAnnotation, let url = task.originalRequest!.url  {
-                Text(transactions.last!.chartDescription)
+            if showAnnotation, let description = transactions.last?.chartDescription(url: task.originalRequest?.url)  {
+                Text(description)
                     .font(.system(size: 6))
             } else {
                 EmptyView()
@@ -111,10 +111,12 @@ struct RequestRow: ChartContent {
 }
 
 extension NetworkTransactionMetricsEntity {
-    var chartDescription: String {
-        String(format: "%@, %.2f",
-               URL(string: url)?.path() ?? url,
-               duration)
+    func chartDescription(url: String?) -> String? {
+        guard let url = url, let url = URL(string: url) else { return nil }
+        
+        return String(format: "%@, %.2f",
+                      url.path,
+                      duration)
     }
     var duration: TimeInterval {
         responseEndDate!.timeIntervalSince1970 - fetchStartDate!.timeIntervalSince1970
